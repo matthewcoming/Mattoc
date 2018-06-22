90428fb34cba22a054251e123b6b99bf4472f5b9
Matthew Coming <Coming.matthew@gmail.com>
2018-06-21 21:33:18 -0500

    expiramental commit

---
+++ project/project_header.py
@@ -0,0 +1,22 @@
+#Matthew Coming test project to learn git-flow
+import sys
+
+"""making a car"""
+class Car(object):
+
+    """initializing the car object"""
+    def _init_(self, company, engine, interior, transmission, color):
+        self.company = company
+        self.engine = engine
+        self.interior = interior
+        self.transmission = transmission
+        self.color = color
+
+    """---class methods---"""
+
+    """starts the car"""
+    def start(self):
+        sys.stdout.write("starting engine")
+        for i in range(2):
+            time.sleep(.500)
+            sys.stdout.write(".")
