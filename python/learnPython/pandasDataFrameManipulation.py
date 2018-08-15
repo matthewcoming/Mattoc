2a9f214f311210198c11d12615936a52a4e92f6a
Matthew Coming <coming.matthew@gmail.com>
2018-08-15 12:12:04 -0700

    pep8 style changes

---
+++ python/learnPython/pandasDataFrameManipulation.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+# a pandas dataframe created with a dict
+
+dictFrame = pd.DataFrame({
+        'AAA': pd.Series(1, index=list(range(4)), dtype='int32'),
+        'BBB': np.array([3] * 4, dtype='int32'),
+        'CCC': pd.Categorical(['this', 'is', 'a', 'categorical']),
+        'DDD': [1, 2, 3, 4],
+        'EEE': np.random.rand(4).tolist()},  # nparray to list
+        ['first', 'second', 'third', 'fourth'])
+
+print(dictFrame.to_string())
+print()
+print()
+
+spacedNum = np.linspace(0.0, 2*np.pi, num=100, endpoint=True)
+spacedOne = np.linspace(-1.0, 1.0, num=100, endpoint=True)
+
+
+sineDict = {'sin': pd.Series(np.sin(spacedNum), index=spacedNum),
+            'cos': pd.Series(np.cos(spacedNum), index=spacedNum),
+            'arcsin': pd.Series(np.arcsin(spacedOne), index=spacedOne),
+            'arccos': pd.Series(np.arccos(spacedOne), index=spacedOne)
+            }
+
+df = pd.DataFrame(sineDict)
+
+tickMarks = pd.Series([0, np.pi/2, np.pi, 3*np.pi/2, 2*np.pi])
+
+df.plot(subplots=True, xticks=tickMarks, sharex=True)
+
+plt.show()
+
+TupleDict = pd.DataFrame({
+            ('Andrew', 'Profit'): {
+                ('First Yr', 'Personal'):   100,
+                ('First Yr', 'Company'):    200,
+                ('Second Yr', 'Personal'):  300,
+                ('Second Yr', 'Company'):   300
+                },
+
+            ('Andrew', 'Expend'): {
+                ('First Yr', 'Personal'):   101,
+                ('First Yr', 'Company'):    201,
+                ('Second Yr', 'Personal'):  301,
+                ('Second Yr', 'Company'):   301}})
+
+print(TupleDict)
--- python/myModule/createCar.py
+++ python/myModule/createCar.py
@@ -1,5 +1,7 @@
-#Matthew Coming test code to learn git-flow, RST, PEP257(docstrings), and creation of
-#python modules
+# Matthew Coming test code to learn git-flow, RST, PEP257(docstrings),
+# and creation of python modules
+
+
 """Make a car.
 
 Module level docstrings appear as the first "statement" in a module. This first
@@ -9,7 +11,8 @@ This module allows you to make a car, then start and stop the car.
 
 """
 class Car:
-    
+
+
     """define the Car class.
 
     First, note that all docstrings begine with a one line summary that ends in
@@ -17,15 +20,15 @@ class Car:
 
     Second, note that the Class level opening and closing quotes are preceded
     and followed by a blank line, respectively. No other docstring should be
-    preceded or followed by anything other than code. 
+    preceded or followed by anything other than code.
 
     """
-    
+
     def __init__(self, make, engine, transmission, color):
         """initialize car object.
-        
+
         Method level docstring to explain how the __init__ method works.
-       
+
        """
         self.make = make
         self.engine = engine
@@ -52,13 +55,13 @@ class Car:
     def getEngineRunning(self):
         """Return state of engine. """
         return self.engineRunning
-    
+
     def start(self):
         """Start the car.
 
-        car.start() first evaluates if the bool variable `engineRunning` is true. If
-        true, print a warning and return. If false, request user input and
-        display that the car has started.
+        car.start() first evaluates if the bool variable `engineRunning` is
+        true. If true, print a warning and return. If false, request user
+        input and display that the car has started.
 
         """
         if self.engineRunning == 1:
@@ -74,7 +77,7 @@ class Car:
         car.start() first evaluates if the bool variable `engineRunning is
         false. If false, print a warning that the car is already on and return.
         If false, request user input and display that the car has stopped.
-        
+
         """
         if self.engineRunning == 0:
             print("Car is already on")
